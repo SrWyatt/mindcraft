387953fdd4e1ed4ed6fb47bf2bef622bd41de624
SrWyatt <esau_batres2001@hotmail.com>
2024-05-23 22:27:49 -0600

    Redimension general

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ec133\Desktop\mindcraft\mindcraft\env
+set VIRTUAL_ENV=C:\Users\Josué\Desktop\proyecto\mindcraft\env
 
 if not defined PROMPT set PROMPT=$P$G
 
